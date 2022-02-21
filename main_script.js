0e93cadf6b387eb99ad2cae5dd9bc5f889812889
Buisson Camille <45961959+Liakhoo@users.noreply.github.com>
2022-02-21 12:19:37 +0100

    Create main_script.js
    début du script

---
+++ main_script.js
@@ -0,0 +1,11 @@
+//importation des fonctions utiles
+//Pour la carte
+france = d3.json("https://france-geojson.gregoiredavid.fr/repo/regions.geojson")
+d3geo = require("d3-geo-projection")
+
+//Pour le scatterplot
+
+
+//Pour le line chart
+
+
