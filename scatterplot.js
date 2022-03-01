ad803eb7d86f11f66f1ca2ee4d6d30b06bc90223
Lyspa <62559198+Lyspa@users.noreply.github.com>
2022-03-01 15:30:45 +0100

    Garder regions multiples quand action filtre

--- scatterplot.js
+++ scatterplot.js
@@ -6,7 +6,6 @@ async function scatterplot(var_x = "recette", var_y = "freq") {
 
   //Préparation données
   let date = d3.select("#rangeSlider").property("value");
-  let choices = chosen_region[chosen_region.length-1]
 
   let data = await getDataPromise();
   let dataset = filterTaille(data);
@@ -135,7 +134,7 @@ async function scatterplot(var_x = "recette", var_y = "freq") {
     .attr("r", d => taille(d["etab"]))
     .attr("fill", d => color(d.region))
     .attr("stroke", "black")
-    .style("opacity",d => isClicked ? (regionMap.get(d.region) == choices ? 1 : 0.15) : 1)
+    .style("opacity",d => isClicked ? (chosen_region.includes(regionMap.get(d.region)) ? 1 : 0.15) : 1)
     .on("mouseover", function(d) {
         if (!isClicked){
           mouseOverScatter(regionMap.get(d.region));
@@ -164,7 +163,7 @@ async function scatterplot(var_x = "recette", var_y = "freq") {
       .attr("width", d => 2*taille(d["etab"]))
       .attr("fill", d => color(d.region))
       .attr("stroke", "black")
-      .style("opacity",d => isClicked ? (regionMap.get(d.region) == choices ? 1 : 0.15) : 1)
+      .style("opacity",d => isClicked ? (chosen_region.includes(regionMap.get(d.region)) ? 1 : 0.15) : 1)
       .on("mouseover", function(d) {
         if (!isClicked){
           mouseOverScatter(regionMap.get(d.region));
@@ -190,7 +189,7 @@ async function scatterplot(var_x = "recette", var_y = "freq") {
             return String(x(d[var_x]) - taille(d["etab"])) + "," + String(y(d[var_y]) + taille(d["etab"])) + " " + String(x(d[var_x])) + "," + String(y(d[var_y]) - taille(d["etab"])) + " " + String(x(d[var_x]) + taille(d["etab"])) + "," + String(y(d[var_y]) + taille(d["etab"]));
       })
       .attr("fill", d => color(d.region))
-      .style("opacity",d => isClicked ? (regionMap.get(d.region) == choices ? 1 : 0.15) : 1)
+      .style("opacity",d => isClicked ? (chosen_region.includes(regionMap.get(d.region)) ? 1 : 0.15) : 1)
       //.attr("stroke", "black")
       .on("mouseover", function(d) {
         if (!isClicked){
